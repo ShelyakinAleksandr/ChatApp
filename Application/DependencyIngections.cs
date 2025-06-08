f415db215d60f5fc05762a0be469ca9617a11d28
Alex <Alex@MEGAPC>
2025-06-08 14:12:21 +0300

    Добавление классов DependencyIngections в проекты Application и Infrastructure

---
+++ Application/DependencyIngections.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyIngections
+    {
+        public static IServiceCollection AddApliication(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
